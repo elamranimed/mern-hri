47682f5abcfaa7118e317a4df5e654ade2a93980
elamranimed <elamranimed84@gmail.com>
2025-04-20 00:23:11 +0100

    produits en grille

--- frontend/src/components/ProductShowcase.js
+++ frontend/src/components/ProductShowcase.js
@@ -21,18 +21,33 @@ const ProductShowcase = () => {
   return (
     <div className="product-showcase">
       <h2>Produits disponibles</h2>
+      {/*
       <ul>
-        
             {produits.map((produit) => (
         <li key={produit._id} className="product-item">
             <img src={`http://localhost:3000/${produit.image}`} alt={produit.title} style={{ width: '100px', height: '100px' }} />
             <strong>{produit.title}</strong>
             <p>Ajouté par : {produit.user?.fullName || 'Vendeur inconnu'}</p>
-            <p>Numéro de téléphone : {produit.user?.phone || 'Non disponible'}</p>
+            <p>Numéro de téléphone : {produit.user?.phone || 'Non disponible'}</p>         
         </li>
         ))}
-
       </ul>
+      */}
+          <div className="product-grid">
+
+            {produits.map((produit) => (
+              <div key={produit._id} className="product-item">
+                
+              <img src={`http://localhost:3000/${produit.image}`} alt={produit.title} style={{ width: '100px', height: '100px' }} />
+              <strong>{produit.title}</strong>
+              <p>Ajouté par : {produit.user?.fullName || 'Vendeur inconnu'}</p>
+              <p>Numéro de téléphone : {produit.user?.phone || 'Non disponible'}</p>
+
+              </div>
+
+            ))}
+          </div>
+
     </div>
   );
 };
